c625e226e588dab12a4ee3e532bcb9264b2ed24d
jayden.lee <jayden.lee@seerstech.com>
2021-12-03 10:13:27 +0900

    1. AssemblyInfoUtil    - Setup 파일 자동 버전업 2. Setup 3. Installer

---
+++ AssemblyInfoUtil/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using Common.Utilities;
+
+namespace AssemblyInfoUtil
+{
+    class Program
+    {
+        private static string fileName = string.Empty;
+        private static string setupfileName = string.Empty;
+
+        /// <summary>
+        /// AssemblyInfoUtil.exe -setup:"C:\Program Files\MyProject1\Setup1\Setup1.vdproj" -ass:"C:\Program Files\MyProject1\AssemblyInfo.cs"
+        /// 
+        /// AssemblyInfoUtil.exe -setup:"$(ProjectDir)$(TargetName)_Setup\$(TargetName)_Setup.vdproj" -ass:"$(TargetPath)"
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length <= 0)
+                {
+                    args = new string[] {
+                        "-setup:" + @"D:\Works\Commons\Common\Setup\Setup.vdproj",
+                        "-ass:" + @"D:\Works\Commons\Common\Output\Common.Demo.exe"
+                    };
+                }
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i].StartsWith("-setup:"))
+                    {
+                        setupfileName = args[i].Substring("-setup:".Length);
+                    }
+                    else if (args[i].StartsWith("-ass:"))
+                    {
+                        fileName = args[i].Substring("-ass:".Length);
+                    }
+                }
+
+                if (File.Exists(setupfileName) && File.Exists(fileName))
+                {
+                    //var setup = File.ReadAllText(setupfileName);
+                    //var json = SerializeHelper.DeserializeByJson(setup);
+
+                    //Jeremy Thompson showing how to detect "ProductVersion" = "8:1.0.0" in vdproj
+                    string setupproj = File.ReadAllText(setupfileName);
+                    int startPosOfProductVersion = setupproj.IndexOf("\"ProductVersion\" = \"") + 20;
+                    int endPosOfProductVersion = setupproj.IndexOf(Environment.NewLine, startPosOfProductVersion) - startPosOfProductVersion;
+                    string versionStr = setupproj.Substring(startPosOfProductVersion, endPosOfProductVersion);
+                    string oldVersionStr = versionStr.Replace("\"", string.Empty).Replace("8:", string.Empty);
+
+                    var info = new FileInfo(fileName);
+                    Assembly _asm = Assembly.LoadFile(fileName);
+                    //var version = _asm.CustomAttributes.FirstOrDefault(f => f.ConstructorArguments);
+                    var version = _asm.CustomAttributes.FirstOrDefault(f => f.AttributeType == typeof(AssemblyInformationalVersionAttribute));
+                    string newVer = version.ConstructorArguments.FirstOrDefault().Value.ToString();
+
+                    var newversionStr = versionStr.Replace(oldVersionStr, newVer);
+                    var newSetupProj = setupproj.Replace(versionStr, newversionStr);
+                    //File.WriteAllText(setupfileName, setupproj);
+
+                    var bak = string.Format("{0}.{1}.bak", setupfileName, oldVersionStr);
+                    if (File.Exists(bak))
+                        File.Delete(bak);
+
+                    var arrBt = Encoding.Default.GetBytes(newSetupProj);
+                    File.Move(setupfileName, bak);
+                    using (var file = new FileStream(setupfileName, FileMode.CreateNew, FileAccess.ReadWrite))
+                    {
+                        file.Write(arrBt, 0, arrBt.Length);
+                        file.Flush();
+                    }
+                }
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(LogTypes.Exception, "", ex);
+            }
+        }
+    }
+}
---
+++ Common.Demo/Installer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Common.Demo
+{
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        public Installer()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+        }
+
+        protected override void OnCommitted(IDictionary savedState)
+        {
+            base.OnCommitted(savedState);
+        }
+
+        protected override void OnCommitting(IDictionary savedState)
+        {
+            base.OnCommitting(savedState);
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        protected override void OnAfterInstall(IDictionary savedState)
+        {
+            base.OnAfterInstall(savedState);
+        }
+
+        protected override void OnAfterRollback(IDictionary savedState)
+        {
+            base.OnAfterRollback(savedState);
+        }
+
+        protected override void OnAfterUninstall(IDictionary savedState)
+        {
+            base.OnAfterUninstall(savedState);
+
+            try
+            {
+                // 모든 폴더 삭제
+                var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                if (Directory.Exists(path))
+                    Directory.Delete(path, true);
+            }
+            catch (Exception) {
+            }
+        }
+
+        protected override void OnBeforeInstall(IDictionary savedState)
+        {
+            base.OnBeforeInstall(savedState);
+        }
+
+        protected override void OnBeforeRollback(IDictionary savedState)
+        {
+            base.OnBeforeRollback(savedState);
+        }
+
+        protected override void OnBeforeUninstall(IDictionary savedState)
+        {
+            base.OnBeforeUninstall(savedState);
+        }
+    }
+}
